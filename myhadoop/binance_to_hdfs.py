c39eacc8bd05b6bb8cb542e5e37a1a3c376420b6
faycalEdderi <faycmx@live.fr>
2025-09-19 11:33:01 +0200

    changing binance_to_hdfs location and refacto to make it work on python3.5

--- myhadoop/binance_to_hdfs.py
+++ myhadoop/binance_to_hdfs.py
@@ -6,19 +6,19 @@ import csv
 symbols = ["BTCUSDT","SHIBUSDT", "DOGEUSDT","TRUMPUSDT","ETHUSDT","PEPEUSDT","BONKUSDT", "PENGUUSDT"]
 data = []
 for symbol in symbols:
-    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+    url = "https://api.binance.com/api/v3/ticker/price?symbol={}".format(symbol)
     resp = requests.get(url).json()
     record = (resp["symbol"], float(resp["price"]), datetime.utcnow().isoformat())
     data.append(record)
 
 # Écriture dans un fichier CSV local
 csv_path = "/tmp/binance_prices.csv"  # Chemin standard pour un cluster Hadoop
-with open(csv_path, "w", newline="") as csvfile:
+with open(csv_path, "w") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["symbol", "price", "timestamp"])
-    writer.writerows(data)
+    for row in data:
+        writer.writerow(row)
 
-print(f"Fichier {csv_path} créé avec succès.")
-
+print("Fichier {} cree avec succes.".format(csv_path))
 # Commande à exécuter sur le cluster Hadoop pour copier dans HDFS :
-print(f"hdfs dfs -mkdir -p /user/binance && hdfs dfs -put -f {csv_path} /user/binance/")
+print("hdfs dfs -mkdir -p /usr/binance && hdfs dfs -put -f {} /usr/binance/".format(csv_path))
